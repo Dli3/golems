1eac80ff57f483b08cee8bf7fac274c756e2d39d
denli <denli@pop-os.localdomain>
2020-08-02 13:21:49 -0700

    Finished the action_cards module. Created actions cards dictionary.

--- main.py
+++ main.py
@@ -1,1 +1,52 @@
-import action_cards
+from action_cards import*
+
+actions_dict = {
+    'minus2yellow_plus2green': minus2yellow_plus2green,
+    'minus2yellow_plus1blue': minus2yellow_plus1blue,
+    'minus3yellow_plus1pink': minus3yellow_plus1pink,
+    'minus3yellow_plus1green1blue': minus3yellow_plus1green1blue,
+    'minus4yellow_plus1blue1pink': minus4yellow_plus1blue1pink,
+    'minus5yellow_plus2pink': minus5yellow_plus2pink,
+    'minus5yellow_plus3blue': minus5yellow_plus3blue,
+    'minus4yellow_plus2blue': minus4yellow_plus2blue,
+    'minus3yellow_plus3green': minus3yellow_plus3green,
+    'minus1green_plus3yellow': minus1green_plus3yellow,
+    'minus2green_plus2blue': minus2green_plus2blue,
+    'minus2green_plus1pink2yellow': minus2green_plus1pink2yellow,
+    'minus2green_plus1blue3yellow': minus2green_plus1blue3yellow,
+    'minus3green_plus1pink1blue1yellow': minus3green_plus1pink1blue1yellow,
+    'minus3green_plus2blue2yellow': minus3green_plus2blue2yellow,
+    'minus3green_plus2pink': minus3green_plus2pink,
+    'minus3green_plus3blue': minus3green_plus3blue,
+    'minus1green1yellow_plus1pink': minus1green1yellow_plus1pink,
+    'minus1blue_plus2green': minus1blue_plus2green,
+    'minus1blue_plus1green4yellow': minus1blue_plus1green4yellow,
+    'minus1blue_plus2green1yellow': minus1blue_plus2green1yellow,
+    'minus2blue_plus2pink': minus2blue_plus2pink,
+    'minus2blue_plus1pink2green': minus2blue_plus1pink2green,
+    'minus2blue_plus3green2yellow': minus2blue_plus3green2yellow,
+    'minus2blue_plus1pink1green2yellow': minus2blue_plus1pink1green2yellow,
+    'minus3blue_plus3pink': minus3blue_plus3pink,
+    'minus1blue2yellow_plus2pink': minus1blue2yellow_plus2pink,
+    'minus1pink_plus2blue': minus1pink_plus2blue,
+    'minus1pink_plus3green': minus1pink_plus3green,
+    'minus1pink_plus1blue1green1yellow': minus1pink_plus1blue1green1yellow,
+    'minus1pink_plus2green2yellow': minus1pink_plus2green2yellow,
+    'minus1pink_plus1blue3yellow': minus1pink_plus1blue3yellow,
+    'minus2pink_plus3blue1green1yellow': minus2pink_plus3blue1green1yellow,
+    'minus2pink_plus2blue3green': minus2pink_plus2blue3green,
+    'plus3yellow': plus3yellow,
+    'plus4yellow': plus4yellow,
+    'plus1green1yellow': plus1green1yellow,
+    'plus2green': plus2green,
+    'plus1blue': plus1blue,
+    'plus1blue1yellow': plus1blue1yellow,
+    'plus1pink': plus1pink,
+    'plus1green2yellow': plus1green2yellow,
+    'upgrade3': upgrade3,
+}
+
+print(actions_dict['minus2yellow_plus2green'])
+
+for i, key in enumerate(actions_dict):
+    print(i, key)
--- players.py
+++ players.py
@@ -1,6 +1,6 @@
 from golems import golems_cards
 import random
-from action_cards import plus_two_yellow, upgrade_two, minus2yellow_plus2green
+from action_cards import plus2yellow, upgrade_2
 
 
 class Player():
@@ -13,7 +13,7 @@ class Player():
                              'blue': 0,
                              'pink': 0}
         self.golems = 0
-        self.hand = []
+        self.hand = [plus2yellow, upgrade_2]
         self.discard_pile = []
 
 
@@ -44,16 +44,3 @@ print(player.crystal_cart)
 
 # f = actions['card_1']['action'](player.crystal_cart)
 # print(f)
-
-# player.hand.append(plus_two_yellow)
-# player.hand.append(upgrade_two)
-
-
-card_list = [plus_two_yellow,
-             upgrade_two, minus2yellow_plus2green]
-for card in card_list:
-    print(card)
-    player.hand.append(card)
-    print(player.hand)
-player.hand[2](player.crystal_cart)
-print(player.crystal_cart)
